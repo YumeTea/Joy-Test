add1792c1bc07f043557fe94b974c69eb509ce08
Yume Tea <infernosnake@msn.com>
2021-06-17 17:38:49 -0700

    Initial project setup with working joycon gyro control
    -Requires BetterJoyForCemu
    -GyrotoMouse must be enabled in BetterJoyForCemu
    -Press R2 while running the project to control the raycast attached to the player

---
+++ ideas.gd
@@ -0,0 +1,56 @@
+extends Node
+
+'Input deadzones need to interpolate between deadzone bounds'
+'Camera rig should have a pivot for aiming modes'
+
+
+
+func ideas():
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
